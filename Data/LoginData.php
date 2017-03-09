33f2603adce4cccbb4c32e074ea409250893d0ef
edwnaba <edwnaba@gmail.com>
2017-03-09 08:56:55 -0600

    Merge origin/master
    Conflicts:
    	Presentation/Client/inicio.php

--- Data/LoginData.php
+++ Data/LoginData.php
@@ -6,32 +6,11 @@ include_once 'Data.php';
 class LoginData extends Data {
 
     public function isUser($id, $pass) {
-        if ($id === "admin@gmail.com" && $pass === "admin") {
+        if ($id === "glosolanop@gmail.com" && $pass === "Samagoli12017") {
             return "3";
         } else {
             return "1";
         }
-//        error_reporting(0);
-//        $conn = new mysqli($this->server, $this->user, $this->password, $this->db);
-//        $conn->set_charset('utf8');
-//        if ($conn->connect_errno) {
-//            echo "Falló la conexion a MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error;
-//            exit;
-//        }
-//        $query = "call isUser('" . $id . "','" . $pass . "'" . ",@result)";
-//        if (!$conn->query("SET @result = 0") || !$conn->query($query)) {
-//            $conn->close();
-//            echo "Falló la llamada al procedimiento: (" . $conn->errno . ") " . $conn->error;
-//            exit;
-//        }
-//        if (!($result = $conn->query("SELECT @result as outSP"))) {
-//            $conn->close();
-//            echo "Falló la obtención: (" . $conn->errno . ") " . $conn->error;
-//            exit;
-//        }
-//        $conn->close($conn);
-//        $ret = $result->fetch_assoc();
-//        return $ret['outSP'];
     }
 
 }
